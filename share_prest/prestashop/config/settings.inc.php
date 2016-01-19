a2864a6759fb7da13407eff5efba57d86f3db0e4
yewed <pa.boccard@gmail.com>
2016-01-19 19:20:32 +0100

    DB_PASSWD = ''

--- share_prest/prestashop/config/settings.inc.php
+++ share_prest/prestashop/config/settings.inc.php
@@ -2,7 +2,7 @@
 define('_DB_SERVER_', 'localhost');
 define('_DB_NAME_', 'share');
 define('_DB_USER_', 'root');
-define('_DB_PASSWD_', 'root');
+define('_DB_PASSWD_', '');
 define('_DB_PREFIX_', 'ps_');
 define('_MYSQL_ENGINE_', 'InnoDB');
 define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
