e33dfd2923dd969470364d094c615d9449425a03
yewed <pa.boccard@gmail.com>
2016-01-21 00:21:42 +0100

    ajout liste supplier sans css (index)

--- share_prest/prestashop/controllers/front/IndexController.php
+++ share_prest/prestashop/controllers/front/IndexController.php
@@ -36,11 +36,14 @@ class IndexControllerCore extends FrontController
     {
         parent::initContent();
         $this->addJS(_THEME_JS_DIR_.'index.js');
+        
+        $supplier=new SupplierControllerCore();
+        $supplier->initContent();
 
-        $this->context->smarty->assign(array('HOOK_HOME' => Hook::exec('displayHome'),
+        /*$this->context->smarty->assign(array('HOOK_HOME' => Hook::exec('displayHome'),
             'HOOK_HOME_TAB' => Hook::exec('displayHomeTab'),
             'HOOK_HOME_TAB_CONTENT' => Hook::exec('displayHomeTabContent')
-        ));
+        ));*/
         $this->setTemplate(_PS_THEME_DIR_.'index.tpl');
     }
 }
